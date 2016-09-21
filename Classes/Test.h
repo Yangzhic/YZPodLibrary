3fe981145752a6ba123504742fb1f6a988232c5c
zhi.yang <zhi.yang@reachauto.com>
2016-09-21 17:18:53 +0800

    Initial Commit
    Signed-off-by: zhi.yang <zhi.yang@reachauto.com>

---
+++ Classes/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Demo
+//
+//  Created by yangzhi on 16/9/21.
+//  Copyright © 2016年 yangzhi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
++ (void)test;
+@end
---
+++ Classes/Test.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  Demo
+//
+//  Created by yangzhi on 16/9/21.
+//  Copyright © 2016年 yangzhi. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
++ (void)test {
+    NSLog(@"test");
+}
+
+@end
